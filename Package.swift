dd31a14217750d9b8458605af255534a680d5bdd
Anka <runner@Mac-1735127832869.local>
2024-12-25 12:28:31 +0000

    KMP SPM package release for 0.1.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.8
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/Startappz/apparition-link-sdk/releases/assets/215773237.zip"
+let remoteKotlinChecksum = "dd8cd72fde1395819ffa92ecb8f94ac1837f0ff5b1106fe9e237f1ee38abe8cf"
 let packageName = "ApparitionSDK"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,9 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./library/build/XCFrameworks/release/\(packageName).xcframework"
-        ),
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
     ]
 )
